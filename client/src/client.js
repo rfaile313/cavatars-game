812e7144b51b4158bd640fe4d79828b116226558
Rudy Faile <febesbrasil@gmail.com>
2020-09-09 14:50:41 -0500

    Win:09/09/2020 02:50pm. Initialize git repo

---
+++ client/src/client.js
@@ -0,0 +1,40 @@
+
+
+const writeEvent = (text) => {
+    // <ul> element
+    const parent = document.querySelector('#events');
+
+    // <li> element
+    const el = document.createElement('li');
+    el.innerHTML = text;
+
+    parent.appendChild(el);
+};
+
+const onFormSubmitted = (e) => {
+    e.preventDefault();
+
+    const input = document.querySelector('#chat');
+    const text = input.value;
+    input.value = '';
+
+    sock.emit('message', text);
+};
+
+//writeEvent("string");
+
+const addButtonListeners = () => {
+    ['rock', 'paper', 'scissors'].forEach((id) => {
+        const button = document.getElementById(id);
+        button.addEventListener('click', () => {
+            sock.emit('turn', id);
+        });
+    });
+};
+
+const sock = io();
+
+sock.on('message', writeEvent);
+
+document.querySelector('#chat-form').addEventListener('submit', onFormSubmitted)
+addButtonListeners();
---
+++ server/server.js
@@ -0,0 +1,52 @@
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const CodenameGame = require('./codenames-game');
+const CodenamesGame = require('./codenames-game');
+
+const app = express();
+
+const clientPath = `${__dirname}/../client`
+console.log(`Serving static file from ${clientPath}`);
+// Serve static file to client with express
+app.use(express.static(clientPath));
+// Create http server with express app
+const server = http.createServer(app);
+// socketio
+const io = socketio(server);
+// set waitingPlayer to null to signify single player
+let waitingPlayer = null;
+
+// socket.io work
+io.on('connection', (sock) => {
+
+    sock.emit('message', 'your id is: ' + sock.id);
+
+    if (waitingPlayer){
+        //start a game
+
+        new CodenamesGame(waitingPlayer, sock);
+
+        waitingPlayer = null;
+    } else {
+        waitingPlayer = sock;
+        waitingPlayer.emit('message', 'Waiting for more players...')
+    }
+
+    sock.on('message', (text) => {
+        //io.emit == all clients currently connected
+        io.emit('message', text);
+    });
+
+});
+
+const PORT = 8000; 
+
+server.on('error', (err) => {
+    console.error('Server Error:', err);
+});
+
+server.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+});
