c69530a60b9ff036e0bc74dd37d4abb9f1342681
Rudy Faile <febesbrasil@gmail.com>
2021-01-20 15:09:45 -0600

    Win:01/20/2021 03:09pm. Adding loading screen/bar

--- client/js/game.js
+++ client/js/game.js
@@ -37,6 +37,68 @@ var currentTeamTurn;
 const game = new Phaser.Game(config);
 
 function preload() {
+
+  var progressBar = this.add.graphics();
+  var progressBox = this.add.graphics();
+  progressBox.fillStyle(0x222222, 0.8);
+  progressBox.fillRect(240, 270, 320, 50);
+  
+  var width = this.cameras.main.width;
+  var height = this.cameras.main.height;
+  var loadingText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 50,
+      text: 'Loading...',
+      style: {
+          font: '20px monospace',
+          fill: '#ffffff'
+      }
+  });
+  loadingText.setOrigin(0.5, 0.5);
+  
+  var percentText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 5,
+      text: '0%',
+      style: {
+          font: '18px monospace',
+          fill: '#ffffff'
+      }
+  });
+  percentText.setOrigin(0.5, 0.5);
+  
+  var assetText = this.make.text({
+      x: width / 2,
+      y: height / 2 + 50,
+      text: '',
+      style: {
+          font: '18px monospace',
+          fill: '#ffffff'
+      }
+  });
+
+  assetText.setOrigin(0.5, 0.5);
+  
+  this.load.on('progress', function (value) {
+      percentText.setText(parseInt(value * 100) + '%');
+      progressBar.clear();
+      progressBar.fillStyle(0xffffff, 1);
+      progressBar.fillRect(250, 280, 300 * value, 30);
+  });
+  
+  this.load.on('fileprogress', function (file) {
+      assetText.setText('Loading asset: ' + file.key);
+  });
+
+  this.load.on('complete', function () {
+      progressBar.destroy();
+      progressBox.destroy();
+      loadingText.destroy();
+      percentText.destroy();
+      assetText.destroy();
+  });
+  
+
   this.load.image("tiles", "../assets/576x96-96x96.png");
   this.load.image("confirm", "../assets/button-confirm.png");
   this.load.image("new_game", "../assets/new_game.png");
